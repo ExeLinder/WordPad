ae6b9456f82b1e76a51c2e2013ece2a61a2bd74b
DenisTrigub <denistrigub66@gmail.com>
2025-05-22 13:13:56 +0300

    added Save for button

---
+++ WordPad/Save.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordPad
+{
+    partial class WordPad
+    {
+        public void Save(string text)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            DialogResult result = save.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                File.WriteAllText(save.FileName, text);
+            }
+        }
+    }
+}
